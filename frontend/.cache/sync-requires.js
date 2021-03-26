4739583fe993f58933a301d327f3be388efff065
Vojtěch  Pavelka <vojta.pavelka@gmail.com>
2021-03-26 18:36:01 +0100

    Heroku implementation

---
+++ frontend/.cache/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\vojte\\Documents\\HwHub\\frontend\\src\\pages\\index.js"))
+}
+
